e5d3c3e503035c71f8dab082f5f6d5eac96a76d8
Arjunpuneeth <91412616+Arjunpuneeth@users.noreply.github.com>
2025-09-05 15:23:32 +0530

    Update Example.java

--- src/test/java/com/sample/Example.java
+++ src/test/java/com/sample/Example.java
@@ -12,7 +12,7 @@ public class Example {
 @Test
 public void launchUrl() {
 	System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
-	ChromeOptions options=new ChromeOptions();
+	ChromeOptions options= new ChromeOptions();
 	options.addArguments("--headless");
 	options.addArguments("--no-sandbox");
 	options.addArguments("--disable-dev-shm-usage");
